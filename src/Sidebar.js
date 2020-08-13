0123d28a8c446783ba07becc9e5b10acc75afeff
Kiệt <kietteikdoi@gmail.com>
2020-08-13 21:17:54 +0700

    init

---
+++ src/Sidebar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Sidebar.css";
+import TwitterIcon from "@material-ui/icons/Twitter";
+
+export default function Sidebar() {
+    return (
+        <div>
+            <div className="sidebar">
+                {/* Icon */}
+                <TwitterIcon />
+
+                {/* option */}
+                {/* option */}
+                {/* option */}
+                {/* option */}
+            </div>
+        </div>
+    );
+}
